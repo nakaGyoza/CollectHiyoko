8e4155269b902f06e939997c1ce45d84ebbd359d
nakaGyoza <ns.carbonhead@gmail.com>
2015-02-12 23:48:02 +0900

    やっぱジャンプ制限無し

--- app/src/main/java/com/example/nakajie/collecthiyoko/MainActivity.java
+++ app/src/main/java/com/example/nakajie/collecthiyoko/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.nakajie.collecthiyoko;
 import android.animation.ObjectAnimator;
 import android.graphics.Point;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Display;
@@ -12,7 +13,10 @@ import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.widget.ImageView;
-import android.graphics.Canvas;
+import android.widget.LinearLayout;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -22,8 +26,13 @@ public class MainActivity extends ActionBarActivity {
     Display disp;
     Point dispsize;
 
+
     ObjectAnimator objectAnimator;
 
+    Timer mTimer = null;
+    Handler mHandler;
+
+    LinearLayout hiyokoView;
 
     float disp_width;
     float disp_height;
@@ -31,16 +40,20 @@ public class MainActivity extends ActionBarActivity {
     float hiyo_height;
 
     float hiyoY;
-    int gravity = 5;
+    float shokiY;
+    int gravity = 4;
     int jump_pow = 50;
-    boolean jump_flag;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        hiyoko = (ImageView)findViewById(R.id.hiyoko);
+        hiyoko = (ImageView) findViewById(R.id.hiyoko);
+        hiyokoView = (LinearLayout) findViewById(R.id.hiyokoView);
+
+        mHandler = new Handler();
 
         // WindowManagerのインスタンス取得
         wm = getWindowManager();
@@ -66,37 +79,72 @@ public class MainActivity extends ActionBarActivity {
             this.hiyoko.setVisibility(hiyoko.VISIBLE);
             hiyo_width = this.hiyoko.getWidth();
             hiyo_height = this.hiyoko.getHeight();
-            Log.d("ひよこの横幅","hiyowidth = " + hiyo_width);
+            Log.d("ひよこの横幅", "hiyowidth = " + hiyo_width);
             //ひよこの初期Y座標取得
             hiyoY = hiyoko.getY();
-            Log.d("ひよこのY座標","hiyoY = " + hiyoY);
+            shokiY = hiyoko.getY();
+            Log.d("ひよこのY座標", "hiyoY = " + hiyoY);
         }
         super.onWindowFocusChanged(hasFocus);
-        animateTranslationX(hiyoko);//
+        animateTranslationX(hiyoko);
     }
 
-    public void onDraw(Canvas c){
 
-        if(hiyoY < 0.0){
-            hiyoY = 0;
-        }else if(hiyoY > disp_height - hiyo_height){
-            hiyoY = disp_height - hiyo_height;
-        }
-         hiyoko.setY(hiyoY - jump_pow);
-         Log.d("ひよこのY座標","hiyoY = " + hiyoY);
-         jump_pow -= gravity;
-
-    }
-
-    private void animateTranslationX(ImageView target){
+    private void animateTranslationX(ImageView target) {
         // translationXプロパティを変化させます
-        objectAnimator = ObjectAnimator.ofFloat(target, "translationX",0f,disp_width - hiyo_width);
+        objectAnimator = ObjectAnimator.ofFloat(target, "translationX", 0f, disp_width - hiyo_width);
         // 2秒かけて実行させます
         objectAnimator.setDuration(2000);
 
         objectAnimator.setRepeatCount(Animation.INFINITE);
         objectAnimator.setRepeatMode(Animation.REVERSE);
         objectAnimator.start();
+    }
+
+    public void hiyokoJump(View v) {
+        jump_pow = 50;
+        if (mTimer != null){
+           mTimer.cancel();
+         }
+         mTimer = null;
+
+
+        if (mTimer == null) {
+
+            //タイマーの初期化処理
+            mTimer = new Timer();
+            mTimer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+
+                    mHandler.post(new Runnable() {
+                        @Override
+                        public void run() {
+
+                            if (hiyoY <= shokiY && hiyoY >= 0) {
+                                hiyoY -= jump_pow;
+                            }
+
+                            if (hiyoY > shokiY) {
+                                hiyoY = shokiY;
+                                jump_pow = 50;
+                                mTimer.cancel();
+                                mTimer = null;
+
+                            }
+
+                            if (hiyoY < 0) {
+                                hiyoY = 0;
+                                jump_pow = 0;
+                            }
+                            hiyoko.setY(hiyoY);
+                            jump_pow -= gravity;
+                        }
+                    });
+                }
+            },0, 30);
+        }
+
     }
 
 
@@ -121,4 +169,13 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if(mTimer != null){
+            mTimer.cancel();
+        }
+    }
+
 }
